b492f9068c9f5e1c35db4fc1d6d213c8ab1e9e9a
kalevishal <kalevishal1610@gmail.com>
2021-10-29 09:18:59 +0530

    Added: When incorrect file as a input program shoud return a custom exception

--- src/test/java/com/bl/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/com/bl/censusanalyser/CensusAnalyserTest.java
@@ -1,15 +1,33 @@
 package com.bl.censusanalyser;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import java.io.IOException;
+
 public class CensusAnalyserTest {
 
     private String INDIA_CENSUS_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
     private String INDIA_STATE_CSV_FILE_PATH = "./src/main/resources/IndiaStateCode.csv";
+    private String INIDAN_CENSUS_WrongCSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
 
     @Test
-    public void givenIndianCensusCSVFIle_WhenLoad_ShouldReturnCorrectRecords() {
+    public void givenIndianCensusCSVFIle_WhenLoad_ShouldReturnCorrectRecords() throws CensusAnalyserException {
         CensusAnalyzer censusAnalyzer = new CensusAnalyzer();
         int count = censusAnalyzer.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
         Assert.assertEquals(29, count);
+    }
+
+    @Test
+    public void givenIndianCensusWrongCSVFile_WhenLoad_ShouldReturnException() {
+        try {
+            CensusAnalyzer censusAnalyser = new CensusAnalyzer();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.loadIndiaCensusData(INIDAN_CENSUS_WrongCSV_FILE_PATH);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(e.type, CensusAnalyserException.ExceptionType.CENSUS_FILE_INCORRECT);
+            e.printStackTrace();
+        }
+
     }
 }
